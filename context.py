3614be2e2772b2844f85fa8b73ac6ac6795d7d65
Ethan-Wu-juniper <ethan@gliacloud.com>
2025-03-13 19:16:12 +0800

    experiment context

---
+++ context.py
@@ -0,0 +1,36 @@
+import asyncio
+from dataclasses import dataclass
+from agents import Agent, RunContextWrapper, Runner, function_tool
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@dataclass
+class UserInfo:  
+    name: str
+    uid: int
+
+@function_tool
+async def fetch_user_age(wrapper: RunContextWrapper[UserInfo]) -> str:  
+    return f"User {wrapper.context.name} is 47 years old"
+
+async def main():
+    user_info = UserInfo(name="John", uid=123)  
+
+    agent = Agent[UserInfo](  
+        name="Assistant",
+        # 把工具拿掉 llm 就讀不到 context 了
+        tools=[fetch_user_age],
+    )
+
+    result = await Runner.run(
+        starting_agent=agent,
+        input="What is the age of the user?",
+        context=user_info,
+    )
+
+    print(result.final_output)  
+    # The user John is 47 years old.
+
+if __name__ == "__main__":
+    asyncio.run(main())
